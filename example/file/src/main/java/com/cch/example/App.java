780919297af7d8f4cc385143e0a5ad39394b76dd
CCH0124 <cchong0124@gmail.com>
2024-06-08 19:32:49 +0800

    add fil component

--- example/file/src/main/java/com/cch/example/App.java
+++ example/file/src/main/java/com/cch/example/App.java
@@ -3,12 +3,30 @@
  */
 package com.cch.example;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
 public class App {
     public String getGreeting() {
         return "Hello World File!";
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    public static void main(String[] args) throws Exception {
+        CamelContext context = new DefaultCamelContext();
+        context.addRoutes( 
+            new RouteBuilder() {
+                public void configure() throws Exception {
+                    from("file:data/inbox?noop=true")
+                    .to("file:data/outbox");
+                };
+            }
+           
+        );
+
+        context.start();
+        Thread.sleep(10000);
+        context.stop();
     }
 }
